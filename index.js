95222de4f33a8176b5561125736bc986b2b4bce3
TwoHundredFaces <emersonsoares141@gmail.com>
2024-03-06 11:27:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Emerson Soares'); //"" - String = Texto
+console.log("'Emerson' Soares");
+console.log(`'Emerson' "Soares"`); // Template Strings
+
+
+console.log(123456, 12.85); // Valor literal = Number, não quebra a linha pois está seguido de vírgula.
+
+
